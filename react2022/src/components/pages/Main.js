7a99d2f9d7689815ac6b86eeea1c2648c24d7706
sungilRyuu <x7nxc3@gmail.com>
2022-04-19 10:43:41 +0900

    0419

--- react2022/src/components/pages/Main.js
+++ react2022/src/components/pages/Main.js
@@ -2,17 +2,65 @@ import React from "react";
 import Header from "../layout/Header";
 import Contents from "../layout/Contents";
 import MainCont from "../includes/MainCont";
+import Loading from "../basics/Loading";
 import Footer from "../layout/Footer";
+// import {gsap} from "gsap";
 
-function Main(){
-    return (
-        <>
-            <Header />
-            <Contents>
-                <MainCont />
-            </Contents>
-            <Footer />
-        </>
-    )
+// function Main(){
+//     return (
+//         <>
+//             <Header />
+//             <Contents>
+//                 <MainCont />
+//             </Contents>
+//             <Footer />
+//         </>
+//     )
+// }
+
+// 로딩 소스를 줄거임 함수 -> 클래스
+// class: 함수의 집합체
+
+class Main extends React.Component {
+    state = {
+        isLoading: true,
+    }
+    
+    mainAnimation = () => {
+      gsap.set(".main__inner", {opacity: 0})
+    }
+
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({isLoading:false});
+            this.mainAnimation();
+        },3000);
+    }
+
+    render(){
+        const {isLoading} = this.state;
+        
+        return (
+            <>
+                {isLoading ? (
+                    <Loading />
+                    ) : (
+                        <>
+                            <Header />
+                            <Contents>
+                                <MainCont />
+                            </Contents>
+                            <Footer />
+                        </>
+                        )
+                }
+            </>
+        )
+    }
 }
+
+
+
+
+
 export default Main;
